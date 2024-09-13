a3b44444e31a60d5cce006e502b4e980f4b925b4
Rashid <rashidkadisha@MacBook-Air-de-Rashid.local>
2024-09-13 17:29:59 +0200

    Initial commit

---
+++ HypothenusCalculatorTraining/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Veuillez saisir coté 1: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Veuillez saisir coté 2: ");
+double b = double.Parse(Console.ReadLine());
+
+double hypothenus = Math.Sqrt((Math.Pow(a, 2) + Math.Pow(b, 2)));
+
+Console.WriteLine($"Si le coté 1 est {a}cm et le coté 2 est {b}cm alors l'hypothénus est {hypothenus:F2}cm");
